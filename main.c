e0b5d93d8f1d3554a174bd753567264593763aee
Felipe Pena <felipensp@gmail.com>
2012-06-06 14:18:17 -0300

    - Simplify code

--- main.c
+++ main.c
@@ -1,14 +1,37 @@
+/**
+ * Copyright (c) 2012, Felipe Pena
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *   Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 #include "cmd.h"
 
-void px_quit_handler(void) {
-	printf("quit!\n");
-	exit(0);
-}
-
-static px_command commands[] = {
+/**
+ * Commands
+ */
+static const px_command commands[] = {
 	{ PX_STRL("quit"), px_quit_handler },
 	{ NULL, 0, NULL }
 };
@@ -16,9 +39,9 @@ static px_command commands[] = {
 /**
  * Handles the command execution
  */
-void handle_cmd(char *cmd, size_t cmd_len) {
+void handle_cmd(char *cmd) {
 	char *params, *op = strtok_r(cmd, " ", &params);
-	px_command *cmd_ptr = commands;
+	const px_command *cmd_ptr = commands;
 
 	while (cmd_ptr->cmd) {
 		if (memcmp(op, cmd_ptr->cmd, cmd_ptr->cmd_len) == 0) {
@@ -28,30 +51,38 @@ void handle_cmd(char *cmd, size_t cmd_len) {
 	}
 
 	printf("Command not found!\n");
+	fflush(stdin);
 }
 
 int main(int argc, char **argv) {
-	char cmd[PX_MAX_CMD_LEN+1];
-	char *cmd_ptr = cmd;
-	char c;
+	char cmd[PX_MAX_CMD_LEN];
+	int ignore = 0, cmd_len;
 
 	memset(cmd, 0, PX_MAX_CMD_LEN+1);
 
 	printf(PX_PROMPT);
 
-	while (1) {
-		c = getchar();
+	while (fgets(cmd, PX_MAX_CMD_LEN, stdin) != NULL) {
+		cmd_len = strlen(cmd) - 1;
 
-		if (c == '\n') {
-			if (cmd_ptr != cmd) {
-				*cmd_ptr = '\0';
-				handle_cmd(cmd, cmd_ptr - cmd);
-				cmd_ptr = cmd;
+		if (ignore == 1) {
+			if (cmd[cmd_len] == '\n') {
+				ignore = 0;
 			}
-			printf(PX_PROMPT);
 			continue;
 		}
-		*cmd_ptr++ = c;
+
+		if (cmd[0] != '\n') {
+			if (cmd[cmd_len] != '\n') {
+				ignore = 1;
+			} else {
+				cmd[cmd_len] = '\0';
+			}
+			handle_cmd(cmd);
+		} else {
+			ignore = 0;
+		}
+		printf(PX_PROMPT);
 	}
 
 	return 0;
